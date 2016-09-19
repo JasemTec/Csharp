f6ad556a2ce84473d41626581b3adb9590c4f427
JASEM <JASEM@>
2016-09-20 02:21:24 +0430

    Add project files.

---
+++ awal barnamaj/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace awal_barnamaj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello worlt");
+            Console.ReadKey();
+        }
+    }
+}
